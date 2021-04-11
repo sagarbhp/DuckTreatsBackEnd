5e87c3da10f11fb47218ce00aaa078caf36bde89
Roy <sagarbhp@gmail.com>
2021-04-10 22:15:42 -0400

    [add] Completed /popular-food route and its passing the test. Initial server complete

--- Routes/publicGetRoutes.js
+++ Routes/publicGetRoutes.js
@@ -7,7 +7,29 @@ const Input = mongoose.model("Input");
 
 publicGetRoutes.get("/popular-food", async (req, res) => {
   console.log("received request get data from database");
-  res.status(300).send("Error");
+  let output = {};
+  try {
+    //Setting up aggregation pipeline to group data based on food
+    let food = await Input.aggregate([
+      {
+        $group: {
+          _id: "$food",
+          count: { $sum: 1 },
+          country: { $addToSet: "$country" },
+          ducksFed: { $sum: { $add: "$duckCount" } },
+        },
+      },
+      { $sort: { count: -1 } },
+    ]);
+    output.data = food;
+    //counting the total number of documents
+    let count = await Input.countDocuments({});
+    output.total = count;
+    res.status(200).send(output);
+  } catch (err) {
+    console.log("Encountered error in /popular-food:", err);
+    res.status(500).send({ message: err.message });
+  }
 });
 
 module.exports = publicGetRoutes;
