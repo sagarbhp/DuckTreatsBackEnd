3b7eada3f2f41b35b276fc5a0d0a76affc86f1ec
Roy <sagarbhp@gmail.com>
2021-04-10 21:57:15 -0400

    [add] completed /data post route. now passing both tests

--- Routes/publicPostRoutes.js
+++ Routes/publicPostRoutes.js
@@ -11,7 +11,40 @@ const Schedule = mongoose.model("Schedule");
 
 publicPostRoutes.post("/data", async (req, res) => {
   console.log("Received post request in /data route");
-  res.send("Data Received!");
+  //output is the object that will be returned upon successful completion of process
+  let output = { message: "Successfully saved data!" };
+
+  //processing input data to standardize form
+  let [data, err] = processInputData(req.body);
+
+  if (err) {
+    console.log("Encountered error while processing input data");
+    return res.status(400).send({ message: err.message });
+  }
+
+  if (data.email != "nil" && emailValidator(data.email)) {
+    //Setup schedular for this input data
+    console.log("found email address in input. Saving scheduled data");
+
+    try {
+      let schedule = new Schedule(data);
+      await schedule.save();
+      output.schedularID = schedule._id;
+    } catch (err) {
+      console.log("Encountered error while savaing scheduled data", err);
+      output.message = "Response was saved but failed to set up schedular!";
+    }
+  }
+  try {
+    let input = new Input(data);
+    await input.save();
+    console.log("Successfully saved input document in database");
+    output.inputID = input._id;
+    res.status(201).send(output);
+  } catch (err) {
+    console.log("Error occoured while saving data in database: ", err);
+    res.status(500).send(err);
+  }
 });
 
 module.exports = publicPostRoutes;
